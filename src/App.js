a0530e6807c5c8de151ba38ce7c5eaefa240e602
direcode 2 <direcode@gmail.com>
2016-10-09 02:13:56 -0500

    automatic numbers are running

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    OlaOne : 1
+  };
+
+  this.AddOnClick = this.AddOnClick.bind(this);
+  this.SubOnClick = this.SubOnClick.bind(this);
+}
+
+AddOnClick() {
+    this.setState({OlaOne: this.state.OlaOne +1});
+  }
+
+  SubOnClick() {
+      this.setState({OlaOne: this.state.OlaOne -1});
+    }
+
+
+    componentDidMount() {
+      setInterval(function() {
+          this.setState({OlaOne: this.state.OlaOne +1})
+      }.bind(this), 1000);
+}
+
   render() {
     return (
       <div className="App">
@@ -12,6 +38,8 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <p>{this.state.OlaOne}</p>
+          <button onClick={this.AddOnClick}>Add</button> | <button onClick={this.SubOnClick}>Subtract</button>
         </p>
       </div>
     );
